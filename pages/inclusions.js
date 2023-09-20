2e66712d08efdd129d6643cd79fb83f160a5ede3
Lorenzo Valente <lorenzo.valente3@studio.unibo.it>
2023-09-20 15:26:03 +0200

    javascript file

---
+++ pages/inclusions.js
@@ -0,0 +1,15 @@
+// Create a function to include the menu and footer using JavaScript
+function includeInclusions() {
+    // Include the navigation bar
+    const menuContainer = document.createElement('div');
+    menuContainer.innerHTML = '<nav><ul class="menu"><li><a href="index.html">Lorenzo Valente</a></li><li><a href="biography.html">Biography</a></li><li><a href="education.html">Education</a></li><li><a href="research.html">Research</a></li><li><a href="publications.html">Publications</a></li><li><a href="talks.html">Talks</a></li><li><a href="team.html">Team</a></li></ul></nav>';
+    document.body.prepend(menuContainer);
+
+    // Include the footer
+    const footerContainer = document.createElement('div');
+    footerContainer.innerHTML = '<footer><p>Contact: <a href="mailto:lorenzo.valente3@studio.unibo.it">lorenzo.valente3@studio.unibo.it</a></p></footer>';
+    document.body.appendChild(footerContainer);
+}
+
+// Call the includeInclusions() function to include the menu and footer
+includeInclusions();
