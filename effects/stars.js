8465664661125034b3bff9d3dfb158154fbb7b52
Lorenzo Valente <lorenzo.valente3@studio.unibo.it>
2023-09-21 17:02:56 +0200

    effects- welcome message

--- effects/stars.js
+++ effects/stars.js
@@ -1,33 +1,45 @@
-// JavaScript code for the falling stars effect 
+// JavaScript code for the falling stars effect
+
 
 // Function to create falling stars
 function createStars() {
-    const numberOfStars = 1000; // Number of falling stars
-    const body = document.body;
-  
-    for (let i = 0; i < numberOfStars; i++) {
-      const star = document.createElement('div');
-      star.className = 'star';
-  
-      // Set random position and size for the stars
-      const xy = getRandomPosition();
-      star.style.left = xy[0] + 'px';
-      star.style.top = xy[1] + 'px';
-  
-      // Add the star to the document's body
-      body.appendChild(star);
-    }
+  const numberOfStars = 150; // Number of falling stars
+  const body = document.body;
+
+  for (let i = 0; i < numberOfStars; i++) {
+    const star = document.createElement('div');
+    star.className = 'star';
+
+    // Set random position and size for the stars
+    const xy = getRandomPosition();
+    star.style.left = xy[0] + 'px';
+    star.style.top = xy[1] + 'px';
+
+    // Add the star to the document's body
+    body.appendChild(star);
   }
-  
-  // Function to get a random position within the window
-  function getRandomPosition() {
-    const x = Math.random() * window.innerWidth;
-    const y = Math.random() * window.innerHeight;
-    return [x, y];
+}
+
+// Function to get a random position within the window
+function getRandomPosition() {
+  const x = Math.random() * window.innerWidth;
+  const y = Math.random() * window.innerHeight;
+  return [x, y];
+}
+
+// Hide the welcome message with a fade-out effect
+function hideWelcomeMessage() {
+  const welcomeMessage = document.getElementById('welcome-message');
+  if (welcomeMessage) {
+    welcomeMessage.style.opacity = 0; // Set opacity to 0 for fade-out effect
+    setTimeout(() => {
+      welcomeMessage.style.display = 'none'; // Hide the welcome message
+      createStars(); // Call the function to create falling stars
+    }, 1000); // Delay for 1 second (1000 milliseconds) before hiding
   }
-  
-  // Start the timer after 5 seconds
-  setTimeout(function () {
-    createStars(); // Call the function to create falling stars
-  }, 2000); // in milliseconds (2 seconds)
-  
+}
+
+// Start the timer after 5 seconds
+setTimeout(function () {
+  hideWelcomeMessage();
+}, 5000); // Delay for 5 seconds (5000 milliseconds) before hiding
