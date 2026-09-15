e06e8af92c5314be5e96e6236e09aed6833849fe
LorenzoValente3 <lorenzo.valente@desy.de>
2026-09-15 13:07:41 +0200

    Track segments in the cascade

--- shower.js
+++ shower.js
@@ -9,7 +9,7 @@
     if (!canvas || window.matchMedia('(prefers-reduced-motion: reduce)').matches) return;
     var ctx = canvas.getContext('2d');
     var W = 0, H = 0, last = 0, nextShower = -1e9;
-    var dots = [], tracks = [];
+    var dots = [], tracks = [], segs = [];
     var LIFE = 6, SPEED = 170, MAX_DOTS = 4000, MAX_TRACKS = 96;
     var E_CRIT = 1 / 32, DEPOSIT_STEP = 7;
     var COLORS = ['46,196,214', '46,196,214', '255,181,71', '255,255,255'];
@@ -29,7 +29,7 @@
             x: W * (0.05 + 0.9 * Math.random()), y: -10,
             a: (Math.random() - 0.5) * 0.4,
             e: 1, left: radLength() * (0.6 + 0.8 * Math.random()),
-            since: 0
+            since: 0, lx: 0, ly: 0
         });
     }
 
@@ -41,6 +41,7 @@
         var born = [];
         for (var i = tracks.length - 1; i >= 0; i--) {
             var p = tracks[i];
+            if (p.lx === 0 && p.ly === 0) { p.lx = p.x; p.ly = p.y; }
             var d = SPEED * dt;
             p.x += Math.sin(p.a) * d;
             p.y += Math.cos(p.a) * d;
@@ -48,6 +49,8 @@
             p.since += d;
             if (p.since >= DEPOSIT_STEP && dots.length < MAX_DOTS) {
                 p.since = 0;
+                segs.push({ x1: p.lx, y1: p.ly, x2: p.x, y2: p.y, e: p.e, born: now });
+                p.lx = p.x; p.ly = p.y;
                 dots.push({
                     x: p.x + randn() * 1.2, y: p.y + randn() * 1.2,
                     r: 0.7 + 2.2 * Math.sqrt(p.e), born: now,
@@ -64,7 +67,7 @@
                     born.push({
                         x: p.x, y: p.y,
                         a: p.a + (k ? 1 : -1) * kick * (0.4 + Math.random()),
-                        e: p.e / 2, left: radLength(), since: 0
+                        e: p.e / 2, left: radLength(), since: 0, lx: p.x, ly: p.y
                     });
                 }
             }
@@ -73,10 +76,23 @@
         for (var j = dots.length - 1; j >= 0; j--) {
             if (now - dots[j].born > LIFE) dots.splice(j, 1);
         }
+        for (var m = segs.length - 1; m >= 0; m--) {
+            if (now - segs[m].born > LIFE) segs.splice(m, 1);
+        }
     }
 
     function draw(now) {
         ctx.clearRect(0, 0, W, H);
+        for (var k = 0; k < segs.length; k++) {
+            var g = segs[k];
+            var la = 0.45 * (1 - (now - g.born) / LIFE);
+            ctx.beginPath();
+            ctx.moveTo(g.x1, g.y1);
+            ctx.lineTo(g.x2, g.y2);
+            ctx.lineWidth = 0.5 + 1.2 * Math.sqrt(g.e);
+            ctx.strokeStyle = 'rgba(255,255,255,' + la.toFixed(3) + ')';
+            ctx.stroke();
+        }
         for (var i = 0; i < dots.length; i++) {
             var d = dots[i];
             var a = 0.85 * (1 - (now - d.born) / LIFE);
